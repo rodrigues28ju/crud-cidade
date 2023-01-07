b6738d321aa907f4541116ed29133c87e9361a14
Juliana Rodrigues Nogueira <julianarnogueira28@hotmail.com>
2023-01-07 19:55:01 -0300

    feat: adicionado reponsavidade do menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useDrawerContext } from '../../contexts';
 
 
 interface IMenuLateralProps{
@@ -7,13 +8,15 @@ interface IMenuLateralProps{
 }
 export const MenuLateral: React.FC<IMenuLateralProps>=({children})=>{
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
 
   return (
     <>
-      <Drawer variant="permanent">
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen} >
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
 
-          <Box width="100%" height={theme.spacing(20)}display="flex" alignItems="center" justifyContent="center">
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar sx={{ height: theme.spacing(12), width: theme.spacing(12) }} alt="fantasma confuso" src="https://i.pinimg.com/564x/ab/01/33/ab013354f2caca61b6b883b6ae230ef6.jpg" />
           </Box>
 
@@ -43,7 +46,7 @@ export const MenuLateral: React.FC<IMenuLateralProps>=({children})=>{
      
       </Drawer>
 
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       
       </Box>
