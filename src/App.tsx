6df0ea4ec7b1a9cda14b337fa5eadefeb465bde2
Juliana Rodrigues Nogueira <julianarnogueira28@hotmail.com>
2023-01-07 01:32:23 -0300

    feat: adicionado tema escuro e troca de thema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { ThemeProvider } from "@mui/material";
+
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from './shared/contexts';
+
+
 
 export const App = () =>{
   return (
-    <ThemeProvider theme={LightTheme}> 
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AppThemeProvider>
+         <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    </AppThemeProvider>
+       
+    
+    
      
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import Button from '@mui/material/Button';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+    const {toggleTheme } = useAppThemeContext();
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>teste</Button>} />
+            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>teste</Button>} />
             
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
