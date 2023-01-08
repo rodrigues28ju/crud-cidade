724bbea3e3f2c8080ed23b57fa0deb006199af26
Juliana Rodrigues Nogueira <julianarnogueira28@hotmail.com>
2023-01-08 16:24:58 -0300

    feat: adicionando o botao dark_mode

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 interface IListItemLinkProps{
   to: string;
@@ -42,6 +42,7 @@ export const MenuLateral: React.FC<IMenuLateralProps>=({children})=>{
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
   const {isDrawerOpen, toggleDrawerOpen,drawerOptions} = useDrawerContext();
+  const  {toggleTheme} = useAppThemeContext();
 
   return (
     <>
@@ -66,11 +67,19 @@ export const MenuLateral: React.FC<IMenuLateralProps>=({children})=>{
                   onClick={smDown ? toggleDrawerOpen : undefined}
                 />
               ))}
-
-              
             </List>
+          </Box>
 
+          <Box>
+            <List component="nav">
+              <ListItemButton  onClick={toggleTheme}>
+                <ListItemIcon>
+                  <Icon>dark_mode</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Alternar tema" />
+              </ListItemButton>
 
+            </List>
           </Box>
 
 
@@ -79,7 +88,6 @@ export const MenuLateral: React.FC<IMenuLateralProps>=({children})=>{
 
 
         </Box>
-     
       </Drawer>
 
       <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
