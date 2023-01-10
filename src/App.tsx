44bb9a2632de5d0f38739d8c2ebd07344b065c39
Juliana Rodrigues Nogueira <julianarnogueira28@hotmail.com>
2023-01-10 18:04:33 -0300

    feat: Adicionado traduções do Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from './routes';
-import { MenuLateral } from './shared/components';
+
+import './shared/forms/TraducoesYup';
+
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';
+import { MenuLateral } from './shared/components';
+import { AppRoutes } from './routes';
 
-
-
-export const App = () =>{
+export const App = () => {
   return (
     <AppThemeProvider>
       <DrawerProvider>
         <BrowserRouter>
-          <MenuLateral >
+
+          <MenuLateral>
             <AppRoutes />
           </MenuLateral>
+
         </BrowserRouter>
       </DrawerProvider>
     </AppThemeProvider>
-    
-   
   );
 };
-
-
